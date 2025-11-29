3b954258fcf7190f8ed2a3dd3fa9575cda2f2350
Kacper Gajda <kgajda226@gmail.com>
2025-11-29 20:11:34 +0100

    fix dconf write

--- gnome.sh
+++ gnome.sh
@@ -9,6 +9,9 @@ dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-2 "['<Super>2'
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-3 "['<Super>3']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-4 "['<Super>4']"
 dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-5 "['<Super>5']"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "<Super>Return"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command "ptyxis --new-window"
-dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name "Launch terminal"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings \
+"['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name \
+"Launch terminal"
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command \ 
+"ptyxis --new-window"
