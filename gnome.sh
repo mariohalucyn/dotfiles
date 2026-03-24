0089d5be34f25ece4610a428b45014fc0f231d91
Kacper Gajda <76203863+mariohalucyn@users.noreply.github.com>
2026-03-24 12:13:22 +0100

    Enable running indicator dominant color in gnome.sh

--- gnome.sh
+++ gnome.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
 gsettings set org.gnome.mutter center-new-windows true
+gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-dominant-color true
 dconf write /org/gnome/shell/keybindings/switch-to-application-1 "@as []"
 dconf write /org/gnome/shell/keybindings/switch-to-application-2 "@as []"
 dconf write /org/gnome/shell/keybindings/switch-to-application-3 "@as []"
