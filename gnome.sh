91a39609a60486d8d6f454019d7d4b188bd393b9
Kacper Gajda <76203863+mariohalucyn@users.noreply.github.com>
2026-03-24 01:11:35 +0100

    Enable centering of new windows in GNOME
    Added a setting to center new windows in GNOME.

--- gnome.sh
+++ gnome.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
+gsettings set org.gnome.mutter center-new-windows true
 dconf write /org/gnome/shell/keybindings/switch-to-application-1 "@as []"
 dconf write /org/gnome/shell/keybindings/switch-to-application-2 "@as []"
 dconf write /org/gnome/shell/keybindings/switch-to-application-3 "@as []"
